d87fb5dd3e9ef72da7dec7567a88fec62d7820ff
vaibhavsitaraman <124644941+vaibhavsitaraman@users.noreply.github.com>
2026-03-24 20:17:33 -0400

    Add basic mathematical operations and functions

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -34,6 +34,74 @@ from typing import Callable, Iterable
 
 # TODO: Implement for Task 0.1.
 
+def mul(x: float, y: float):
+    return x * y
+
+
+def id(x: float):
+    return x
+
+
+def add(x: float, y: float):
+    return x + y
+
+
+def neg(x: float):
+    return -x
+
+
+def lt(x: float, y: float):
+    return x < y
+
+
+def eq(x: float, y: float):
+    return x == y
+
+
+def max(x: float, y: float):
+    return x if x > y else y
+
+
+def is_close(x: float, y: float):
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float):
+    if x >= 0:
+        return 1.0 / (1.0 + math.exp(-x))
+    else:
+        ex = math.exp(x)
+        return ex / (1.0 + ex)
+
+
+def relu(x: float):
+    return x if x > 0 else 0.0
+
+
+def log(x: float):
+    return math.log(x)
+
+
+def exp(x: float):
+    return math.exp(x)
+
+
+def inv(x: float):
+    return 1.0 / x
+
+
+def log_back(x: float, y: float):
+    return y / x
+
+
+def inv_back(x: float, y: float):
+    return -y / (x * x)
+
+
+def relu_back(x: float, y: float):
+    return y if x > 0 else 0.0
+
+
 
 # ## Task 0.3
 
